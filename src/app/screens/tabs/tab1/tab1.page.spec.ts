ab5e073efcefa0c5db4c81499a0f12edd0b4f205
Welle77 <simon.wessmann.brok@gmail.com>
2020-09-24 08:31:39 +0200

    New nav style

--- src/app/screens/tabs/tab1/tab1.page.spec.ts
+++ src/app/screens/tabs/tab1/tab1.page.spec.ts
@@ -3,7 +3,6 @@ import { IonicModule } from '@ionic/angular';
 import { ApolloTestingModule } from 'apollo-angular/testing';
 
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 describe('Tab1Page', () => {
   let component: Tab1Page;
@@ -12,7 +11,7 @@ describe('Tab1Page', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule, ApolloTestingModule],
+      imports: [IonicModule.forRoot(), ApolloTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab1Page);
--- src/app/screens/tabs/tab1/tab1.page.ts
+++ src/app/screens/tabs/tab1/tab1.page.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ApolloError } from '@apollo/client/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { IRatesQuery, IRatesQueryVariables, IRatesQuery_rates } from 'src/graphql_interfaces';
 import { RATES_QUERY } from './queries';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page implements OnInit, OnDestroy {
   rates: IRatesQuery['rates'];
-  loading: boolean = true;
-  error: ApolloError;
+  loading: boolean;
+  private querySubscription: Subscription;
 
   constructor(private apollo: Apollo) {}
   ngOnInit() {
-    this.apollo
+    this.querySubscription = this.apollo
       .watchQuery<IRatesQuery, IRatesQueryVariables>({
         query: RATES_QUERY,
         variables: { currency: 'DKK' },
       })
-      .valueChanges.subscribe(({ data, loading, error }) => {
+      .valueChanges.subscribe(({ data, loading }) => {
         this.rates = data.rates;
         this.loading = loading;
-        this.error = error;
       });
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
   }
 }
