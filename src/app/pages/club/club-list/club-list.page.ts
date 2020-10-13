e22bc8dcf623c86df3ca6a95d2bbbe1ed5e374ec
Welle77 <simon.wessmann.brok@gmail.com>
2020-10-13 14:02:11 +0200

    sorting works now

--- src/app/pages/club/club-list/club-list.page.ts
+++ src/app/pages/club/club-list/club-list.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { ClubListQueryService } from 'src/app/services/GRAPHQL/club/club-list-query.service';
 import { IGetClubsQuery } from 'src/graphql_interfaces';
 import { Observable } from 'rxjs';
@@ -12,9 +12,34 @@ import { Observable } from 'rxjs';
 export class ClubListPage implements OnInit {
   constructor(private clubListQuery: ClubListQueryService) {}
 
-  public clubs: Observable<IGetClubsQuery['clubs']>;
+  public clubs$: Observable<IGetClubsQuery['clubs']>;
+  public filteredClubs: IGetClubsQuery['clubs'];
+  private searchTerm: string;
+
+  public search = (event: EventData) => {
+    const { value } = event.target;
+    this.searchTerm = value;
+  };
+
+  public onSearchPressed = () => {
+    requestAnimationFrame(() =>
+      this.clubs$.subscribe(
+        (clubs) =>
+          (this.filteredClubs = clubs!.filter((club) => {
+            if (club && club.name) return club.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+          }))
+      )
+    );
+  };
 
   ngOnInit() {
-    this.clubs = this.clubListQuery.watch().valueChanges.pipe(map((result) => result.data.clubs));
+    this.clubs$ = this.clubListQuery.watch().valueChanges.pipe(map((result) => result.data.clubs));
+    this.clubs$.subscribe((clubs) => (this.filteredClubs = clubs));
   }
 }
+
+type EventData = {
+  target: {
+    value: string;
+  };
+};
