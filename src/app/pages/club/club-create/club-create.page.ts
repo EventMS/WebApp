4a96018c4c3a2ede8d2b5b7ca144e69f1998f07c
Welle77 <simon.wessmann.brok@gmail.com>
2020-10-30 09:31:49 +0100

    the smallest of changes

--- src/app/pages/club/club-create/club-create.page.ts
+++ src/app/pages/club/club-create/club-create.page.ts
@@ -131,7 +131,7 @@ export class ClubCreatePage implements OnInit {
   private handleResponse(data: ICreateClubMutation) {
     this.clubform.reset();
     //Navigate to page for new created club
-    this.router.navigate(['/']);
+    this.router.navigate(Paths.show_club.route(data?.createClub?.clubId!));
   }
 }
 
