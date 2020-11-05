934c409b791c126fdabecf0668277a7eb04d00e7
Jesper <repsej.stroem@gmail.com>
2020-11-05 12:17:30 +0100

    Routing fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
-export const environment: { apiUrl: string, production: boolean } = {
+export const environment: { apiUrl: string, production: boolean, websocketUrl: string } = {
   apiUrl: 'http://eventms/api/',
   production: true,
+  websocketUrl: 'http://eventms/api/websocket/'
 };
