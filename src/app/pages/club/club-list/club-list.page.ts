148781d47cfed2ce7a0e7118c4422ef32f97e6ea
Jesper <repsej.stroem@gmail.com>
2020-11-12 12:57:21 +0100

    Filter on events

--- src/app/pages/club/club-list/club-list.page.ts
+++ src/app/pages/club/club-list/club-list.page.ts
@@ -4,7 +4,7 @@ import { IGetClubsQuery } from 'src/graphql_interfaces';
 import { fromEvent, Observable } from 'rxjs';
 import { ClubService } from 'src/app/services/GRAPHQL/club/club.service';
 import { LoadingController } from '@ionic/angular';
-import { FromEventTarget } from 'rxjs/internal/observable/fromEvent';
+
 @Component({
   selector: 'app-club-list',
   templateUrl: './club-list.page.html',
